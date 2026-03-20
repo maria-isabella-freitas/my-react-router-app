94f1d77bad3db69ea7e28ae34cc65e3d74b54561
freitas <isabella.nunes.de.freitas@student.howest.be>
2026-03-20 15:13:27 +0100

    added some tailwind css (dishes grid)

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -20,17 +20,21 @@ export default function localDishes({
 
     return (
         <section>
-            <h1>{params.countryId} Meals</h1>
+            <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">
+                {params.countryId} Meals
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         
         {loaderData.map(country => (
             <article>
                 <img src={country.strMealThumb} alt={country.strMeal} />
                 <h2>{country.strMeal}</h2>
                 <Link key={country.idMeal} to={`/countries/${country.idMeal}`}>
-                    View Details
+                    Go to Recipe
                 </Link>
             </article>
             ))}
+        </div>
         </section>
     )
 
